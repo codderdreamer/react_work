73f53b69c86a6a8a5921911454dcdf0d8ee8c933
codderdreamer <sevdayucedagg@gmail.com>
2022-10-17 21:02:33 +0300

    first commit

---
+++ hello_react/public/scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// JSX - Javascript XML
+
+var root = document.getElementById('root');
+var template = /*#__PURE__*/React.createElement("h1", {
+  id: "header"
+}, "Hello World");
+ReactDOM.render(template, root);
---
+++ hello_react/src/app.js
@@ -0,0 +1,7 @@
+// JSX - Javascript XML
+
+var root = document.getElementById('root');
+
+var template = <h1 id="header">Hello World</h1>
+
+ReactDOM.render(template,root)
