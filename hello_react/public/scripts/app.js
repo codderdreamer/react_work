197d6c6fe9fa386bc115fdbda9b8825075596511
codderdreamer <sevdayucedagg@gmail.com>
2022-10-18 10:21:28 +0300

    jsx koşul ifadeleri

--- hello_react/public/scripts/app.js
+++ hello_react/public/scripts/app.js
@@ -3,7 +3,35 @@
 // JSX - Javascript XML
 
 var root = document.getElementById('root');
-var template = /*#__PURE__*/React.createElement("h1", {
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "Hello World");
-ReactDOM.render(template, root);
+}, "Hello World"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
+var name = "Samsung 510";
+var price = 5000;
+var description = "iyi bir telefon";
+var product = {
+  // name : "Samsung 510",
+  price: 1000,
+  description: "iyi",
+  types: ['red', 'blue']
+};
+function formatPrice(p) {
+  return p.price + 'TL';
+}
+function getDescription(description) {
+  if (description) {
+    return /*#__PURE__*/React.createElement("p", {
+      id: "product-desc"
+    }, "description: ", description);
+  } else {
+    return 'no description';
+  }
+}
+var template2 = /*#__PURE__*/React.createElement("div", {
+  id: "product-details"
+}, /*#__PURE__*/React.createElement("h2", {
+  id: "product-name"
+}, "name: ", product.name ? product.name : 'no name'), /*#__PURE__*/React.createElement("p", {
+  id: "pruduct-price"
+}, "price: ", product.price == 0 ? 'free' : product.price), product.price && product.price > 0 && /*#__PURE__*/React.createElement("p", null, "price: ", product.price, " TL"), getDescription(product.description), /*#__PURE__*/React.createElement("p", null, product.types.length > 0 ? 'there are options' : 'no options'));
+ReactDOM.render(template2, root);
--- hello_react/src/app.js
+++ hello_react/src/app.js
@@ -2,6 +2,49 @@
 
 var root = document.getElementById('root');
 
-var template = <h1 id="header">Hello World</h1>
+var template = <div>
+                    <h1 id="header">Hello World</h1>
+                    <div>Lorem ipsum dolor sit amet.</div>
+                    <ul>
+                        <li>Lorem, ipsum.</li>
+                        <li>Lorem, ipsum.</li>
+                        <li>Lorem, ipsum.</li>
+                    </ul>
+                </div>;
 
-ReactDOM.render(template,root)
+var name = "Samsung 510";
+var price = 5000;
+var description = "iyi bir telefon"
+
+var product =   {
+                    // name : "Samsung 510",
+                    price : 1000,
+                    description : "iyi",
+                    types : ['red','blue']
+                }
+
+function formatPrice(p) {
+    return p.price + 'TL';
+}
+
+function getDescription(description){
+    if(description){
+        return <p id="product-desc">description: {description}</p>;
+    }
+    else{
+        return 'no description'
+    }
+}
+
+var template2 = (
+    <div id="product-details">
+        <h2 id="product-name">name: {product.name ? product.name:'no name'}</h2>
+        <p id="pruduct-price">price: {product.price==0?'free':product.price}</p>
+        {(product.price && product.price>0) && <p>price: {product.price} TL</p> }
+        {getDescription(product.description)}
+        <p>{product.types.length>0?'there are options':'no options'}</p>
+    </div>
+);
+
+
+ReactDOM.render(template2,root)
